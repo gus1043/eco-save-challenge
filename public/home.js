1d740bb3b7b0e6e5e9a77151179f35d162a7076e
JihyeonChoi <gus1043@gmail.com>
2024-05-25 17:16:59 +0900

    feat : 홈 월 타이틀

---
+++ public/home.js
@@ -0,0 +1,10 @@
+// 현재 월을 표시
+function displayCurrentMonth() {
+    const heading = document.querySelector('h2');
+    const currentDate = new Date();
+    const currentMonthIndex = currentDate.getMonth() + 1; //getMonth()메서드는 zero-base라서 +1해줘야함
+    heading.textContent = currentMonthIndex + '월 챌린지';
+}
+
+// 페이지 로드 시 현재 월 표시
+window.addEventListener('load', displayCurrentMonth);
