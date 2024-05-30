0cff0f002c0901d8d092537703f1cf6e04ff7967
JihyeonChoi <gus1043@gmail.com>
2024-05-30 15:33:55 +0900

    add : router 추가

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,15 @@ const User = require('../models/user');
 
 const router = express.Router();
 
+//메인페이지
 router.get('/', async (req, res, next) => {
-  try {
-    const users = await User.findAll();
-    res.render('sequelize', { users });
-  } catch (err) {
-    console.error(err);
-    next(err);
-  }
+    try {
+        const users = await User.findAll();
+        res.render('sequelize', { users });
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
+    res.render('home', { title: '홈' });
 });
-
 module.exports = router;
