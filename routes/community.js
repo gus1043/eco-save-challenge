9450c092d71268cd373811a8b18cf18ef1c0e883
JihyeonChoi <gus1043@gmail.com>
2024-05-30 20:43:19 +0900

    refactor : 커뮤니티 api 변경

--- routes/community.js
+++ routes/community.js
@@ -2,9 +2,11 @@ const express = require('express');
 const Wiki = require('../models/wiki');
 
 const router = express.Router();
-
+router.route('/').get(async (req, res, next) => {
+    res.render('community', { title: '커뮤니티' });
+});
 router
-    .route('/')
+    .route('/wikis')
     .get(async (req, res, next) => {
         try {
             const wikis = await Wiki.findAll({
@@ -12,7 +14,6 @@ router
                 order: [['created_at', 'DESC']],
                 limit: 1,
             });
-            res.render('community', { title: '커뮤니티' });
             res.json(wikis);
         } catch (err) {
             console.error(err);
@@ -21,7 +22,7 @@ router
     })
     .post(async (req, res, next) => {
         try {
-            const { created_at, content } = req.body;
+            const { created_at, content } = req.body; //입력 내용
 
             if (!content) {
                 return res.status(400).json({ error: 'Content cannot be null' });
