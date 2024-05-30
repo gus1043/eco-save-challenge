c708ca02ef8faec0a42a5d8053496ee044a6db73
JihyeonChoi <gus1043@gmail.com>
2024-05-30 15:32:22 +0900

    style : 타일 색상 변경

--- public/home.js
+++ public/home.js
@@ -1,6 +1,6 @@
 // 현재 월을 표시
 function displayCurrentMonth() {
-    const heading = document.querySelector('h2');
+    const heading = document.getElementById('heading');
     const currentDate = new Date();
     const currentMonthIndex = currentDate.getMonth() + 1; //getMonth()메서드는 zero-base라서 +1해줘야함
     heading.textContent = currentMonthIndex + '월 챌린지';
