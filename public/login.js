dbdbf63ec6f4f1a8c603577f3f2f972d26bb4536
JihyeonChoi <gus1043@gmail.com>
2024-05-30 20:38:08 +0900

    feat : 로그인 여부에 따른 redirect

--- public/login.js
+++ public/login.js
@@ -10,8 +10,8 @@ document.getElementById('form').addEventListener('submit', async (e) => {
         return alert('비밀번호를 입력하세요');
     }
     try {
-        // 서버에 회원가입 요청
-        const response = await axios.post('/users/login', { email, password });
+        // 서버에 로그인 요청
+        const response = await axios.post('/users/login', { email, password }, { withCredentials: true });
         // 성공하면 login으로 이동
         if (response.status === 200) {
             await Swal.fire({
@@ -21,7 +21,15 @@ document.getElementById('form').addEventListener('submit', async (e) => {
                 confirmButtonColor: '#19A337',
                 confirmButtonText: '확인',
             });
-            window.location.href = '/'; // 메인 페이지로 이동
+
+            // 여기서 response.data에 따라 다른 페이지로 리다이렉션하는 로직을 추가
+            if (response.data.address && response.data.address !== 'No residence information found') {
+                // 주소 정보가 있을 경우의 페이지
+                window.location.href = '/';
+            } else {
+                // 주소 정보가 없을 경우의 페이지
+                window.location.href = '/users/residenceInfo';
+            }
         } else {
             // 서버에서 정의된 다른 상태 코드 처리
             await Swal.fire({
